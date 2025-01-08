142a8ea3253e50f8634383a4e87dd5a8264fd110
aryan7889 <aryan.goyalsons28@gmail.com>
2025-01-08 22:44:05 +0530

    created encapsulation

---
+++ part_6/encapsulation.js
@@ -0,0 +1,47 @@
+class BankBalance {
+    #balance = 0;
+
+    deposit(amount){
+        this.#balance += amount
+        return this.#balance
+    }
+
+    getbalance(){
+        return `$ ${this.#balance}`
+    }
+}
+let account = new BankBalance()
+// console.log(account.getbalance);
+
+// abstraction:
+
+class CoffeeMachine{
+
+    start(){
+        return `Starting the machine...`
+    }
+    brewcoffee (){
+        return `brewing the coffee ....`
+    }
+}
+
+let mymachine = new CoffeeMachine()
+console.log(mymachine.start());
+console.log(mymachine.brewcoffee());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
