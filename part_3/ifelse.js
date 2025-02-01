1a1fe2e57b8f7e5307b56d12122c1c15efe7737a
aryan7889 <aryan.goyalsons28@gmail.com>
2025-02-01 14:30:59 +0530

    practice of if and else

--- part_3/ifelse.js
+++ part_3/ifelse.js
@@ -24,5 +24,89 @@ function checking(age) {
         
     }
 }
-checking(25);
-checking(12);
+// checking(25);
+// checking(12);
+
+
+// Write a program to find the largest of two numbers.
+
+function findlargest(num1 ,num2) {
+    if (num1 > num2) {
+        console.log(`${num1} is greater then ${num2}`);
+        
+    }else if (num2 > num1) {
+        console.log(`${num2} is greater then ${num1}`);
+        
+    }else{
+        console.log("both are equal");
+        
+    }
+    
+}
+// findlargest(10, 20);
+// findlargest(20, 10);
+// findlargest(10, 10);
+
+//Write a program that determines whether a given year is a leap year.
+
+function leapyear(year) {
+    if (year % 4 === 0) {
+        console.log( `${year} is the lear year`);    
+    } else {
+        console.log(`${year} is not the leap year`);
+        
+    }
+    
+}
+// leapyear(2020);
+// leapyear(2021);
+
+
+
+// Write a program to check if a number is positive, negative, or zero.
+function checknumber(numbers) {
+    if (numbers > 0) {
+        console.log(`this ${numbers} is positive.`);
+    }else if (numbers < 0) {
+        console.log(`this ${numbers} is negative or less than zero.`);
+    }else{
+        console.log(`number is equal to zero.`);
+        
+    }
+    
+}
+// checknumber(5)
+// checknumber(-1)
+// checknumber(0)
+
+// Problem:
+// Write a program that assigns a grade based on a student’s marks.
+function grades(marks){
+    if (marks >= 90) {
+       console.log(`your ${marks} are greater than 90 so you are given
+        grade A`);}else
+         if (marks >= 80) {
+            console.log(`${marks} == B grade.`);
+        }else if (marks >= 70) {
+            console.log(`GRADE C`);     
+        }else{
+            console.log(`you failed.`);
+            
+        }
+
+}
+// grades(60)
+
+// Write a simple login validation system.
+function login(username, password){
+    let storeduser = "admin"
+    let storedpassword = "abc"
+    if (username === storeduser && password === storedpassword) {
+        console.log(`ready to launch`);
+        
+    } else {
+        console.log(`Invalid username or password.`);
+        
+    }
+}
+login("admin", "abc")
