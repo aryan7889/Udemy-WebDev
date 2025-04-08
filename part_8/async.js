28f61cb60db1393ae5d3dbfb735c1fa48d5e6be3
aryan7889 <aryan.goyalsons28@gmail.com>
2025-04-08 16:14:16 +0530

    done

---
+++ part_8/async.js
@@ -0,0 +1,42 @@
+// Javascript:
+// 1.=>Synchronous------------>first one line of code will be executed then other line 
+//     would be executed it is the behaviour of the synchronous nature of java Script.
+// 
+// 2.=>Single threaded------->only one thread would be used to execute the commad and it is one of the 
+// slowest language in comparison to the new language coming into the market in today world.
+
+
+/*
+execution context :--
+=execute one line of code at a time.
+
+=>each operation waits for the last one to complete the last one.
+
+BLOCKING CODE AND NON BLOCKING CODE :
+
+
+BLOCKING CODE:
+=>Block the flow of the program.
+Ex..let's say if someone say to someone to wait until he comes after taaking the water so the 
+other person cant do anything around he need to only waitfor him to come.
+
+
+Non Blocking Code:
+here there is not blocking anything.
+
+Lets say if you have some file to read in your ocde if you are using
+the blocking code then your program cant do anything until that file is fully read
+inspite of how many time it will take it to read that file while if you are using 
+the non blocking code then thw file would be get read and on the other hand your 
+program can also run hand to hand.
+
+
+
+
+Now here most of us would be saying that the non blocking code is nice it can 
+do both the work syncronisbly but problem arise when you are working with database 
+as it also a file if you have enter the wrong input that database decline to 
+accept that the problem arise so we need to look at what is necessary at what time.
+
+
+*/
